7dd1383a925bff8dae60dee79f0941d8fabbab08
fatemeh soufian <fatemehsoufian2@gmail.com>
2025-02-16 22:27:43 +0200

    mobile computing fourth assignment
    - Add the necessary permissions to the AndroidManifest.xml
    - Register the new ForegroundService in the manifest
    - Use a deep link to navigate to the Add Food screen when tapping on the notification
    - Add the ShakeDetector file, which contains the shake detection service and the show notification function

--- app/src/main/java/org/uni/mobilecomputinghomework1/MainActivity.kt
+++ app/src/main/java/org/uni/mobilecomputinghomework1/MainActivity.kt
@@ -1,9 +1,12 @@
 package org.uni.mobilecomputinghomework1
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import org.uni.mobilecomputinghomework1.ui.theme.MobileComputingHomework1Theme
 
@@ -15,8 +18,18 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MobileComputingHomework1Theme {
-                Navigation()
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    requestPermissions(arrayOf(android.Manifest.permission.POST_NOTIFICATIONS), 0)
+                }
+                val navController = rememberNavController()
+                Navigation(navController)
+                if (intent?.action == "OPEN_ADD_FOOD") {
+                    navController.navigate(Screens.AddFood.route)
+                }
             }
+        }
+        Intent(this, ShakeDetectionService::class.java).also { intent ->
+            startService(intent)
         }
     }
 }
--- app/src/main/java/org/uni/mobilecomputinghomework1/Navigation.kt
+++ app/src/main/java/org/uni/mobilecomputinghomework1/Navigation.kt
@@ -1,16 +1,16 @@
 package org.uni.mobilecomputinghomework1
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navDeepLink
 import org.uni.mobilecomputinghomework1.addfood.AddFoodScreen
 import org.uni.mobilecomputinghomework1.detail.FoodDetailScreen
 import org.uni.mobilecomputinghomework1.home.HomeScreen
 
 @Composable
-fun Navigation() {
-    val navController = rememberNavController()
+fun Navigation(navController: NavHostController) {
     NavHost(navController = navController, startDestination = Screens.Home.route) {
         composable(route = Screens.Home.route) {
             HomeScreen(navController)
@@ -22,7 +22,10 @@ fun Navigation() {
                 navController = navController
             )
         }
-        composable(route = Screens.AddFood.route) {
+        composable(
+            route = Screens.AddFood.route,
+            deepLinks = listOf(navDeepLink { uriPattern = "myapp://addfood" })
+        ) {
             AddFoodScreen(navController = navController)
         }
     }
